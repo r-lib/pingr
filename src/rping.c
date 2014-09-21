7393208de1e553eae1a211679ab7b475c653a47a
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-21 14:51:19 -0400

    Drop paping, rewrite from scratch

--- src/rping.c
+++ src/rping.c
@@ -2,15 +2,30 @@
 #include <R.h>
 #include <Rdefines.h>
 
-int r_ping_main(const char *Destination, int Port, int Type,
-		int Continuous, int Count, int Timeout, double *Values);
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <unistd.h>
 
 SEXP r_ping(SEXP p_destination, SEXP p_port, SEXP p_type, SEXP p_continuous,
 	    SEXP p_count, SEXP p_timeout) {
 
   SEXP result;
-  const char *Destination;
-  int Port, Type, Continuous, Count, Timeout, ret;
+
+  const char *destination;
+  int port, type, continuous, count, timeout;
+
+  struct in_addr ip_address;
+  struct hostent *remote_host = NULL;
+  const char *host_name;
+  int is_ip;
+  int i = 0;
+
+  /* ---------------------------------------------------------------- */
+  /* Check arguments                                                  */
+  /* ---------------------------------------------------------------- */
 
   if (LENGTH(p_destination) != 1) {
     error("destination must be a character scalar");
@@ -23,17 +38,94 @@ SEXP r_ping(SEXP p_destination, SEXP p_port, SEXP p_type, SEXP p_continuous,
   if (LENGTH(p_count) != 1) { error("type must be a numeric scalar"); }
   if (LENGTH(p_timeout) != 1) { error("type must be a numeric scalar"); }
 
-  Destination = CHAR(STRING_ELT(AS_CHARACTER(p_destination), 0));
-  Port = INTEGER(AS_INTEGER(p_port))[0];
-  Type = INTEGER(AS_INTEGER(p_type))[0];
-  Continuous = INTEGER(AS_INTEGER(p_continuous))[0];
-  Count = INTEGER(AS_INTEGER(p_count))[0];
-  Timeout = INTEGER(AS_INTEGER(p_timeout))[0];
+  destination = CHAR(STRING_ELT(AS_CHARACTER(p_destination), 0));
+  port = INTEGER(AS_INTEGER(p_port))[0];
+  type = INTEGER(AS_INTEGER(p_type))[0];
+  if (type == 0) { type = IPPROTO_TCP; } else { type = IPPROTO_UDP; }
+  continuous = INTEGER(AS_INTEGER(p_continuous))[0];
+  count = INTEGER(AS_INTEGER(p_count))[0];
+  timeout = INTEGER(AS_INTEGER(p_timeout))[0];
 
-  PROTECT(result = NEW_NUMERIC(Count));
+  /* ---------------------------------------------------------------- */
+  /* Resolve host                                                     */
+  /* ---------------------------------------------------------------- */
 
-  ret = r_ping_main(Destination, Port, Type, Continuous, Count,
-		    Timeout, REAL(result));
+  remote_host = gethostbyname(destination);
+  if (!remote_host) { error("Cannot resolve host name"); }
+  host_name = remote_host->h_name;
+  is_ip = !strcmp(host_name, destination);
+  ip_address = *(struct in_addr*) remote_host->h_addr_list[0];
+
+  /* ---------------------------------------------------------------- */
+  /* Main ping loop                                                   */
+  /* ---------------------------------------------------------------- */
+
+  PROTECT(result = NEW_NUMERIC(count));
+
+  while (1) {
+
+    /* Try to connect */
+
+    struct timeval tv, start, stop;
+    double t_start, t_stop;
+    struct sockaddr_in c_address;
+    fd_set read, write;
+    int ret;
+    double time;
+
+    int c_socket = socket(AF_INET,
+			  type == IPPROTO_UDP ? SOCK_DGRAM : SOCK_STREAM,
+			  type);
+
+    if (c_socket == -1) { error("Cannot connect to host"); }
+
+    c_address.sin_addr = ip_address;
+    c_address.sin_family = AF_INET;
+    c_address.sin_port = htons(port);
+
+    tv.tv_sec  = timeout / 1000000;
+    tv.tv_usec = timeout % 1000000;
+
+    gettimeofday(&start, NULL);
+
+    connect(c_socket, (const struct sockaddr*) &c_address,
+	    sizeof(c_address));
+
+    FD_ZERO(&read);
+    FD_ZERO(&write);
+    FD_SET(c_socket, &read);
+    FD_SET(c_socket, &write);
+
+    ret = select(c_socket + 1, &read, &write, NULL, &tv);
+
+    if (ret != 1) {
+      close(c_socket);
+      time = NA_REAL;
+    } else {
+      gettimeofday(&stop, NULL);
+      t_start = start.tv_usec + start.tv_sec * 1000000;
+      t_stop = stop.tv_usec + stop.tv_sec * 1000000;
+      time = (t_stop - t_start) / 1000;
+    }
+
+    if (!FD_ISSET(c_socket, &read) && !FD_ISSET(c_socket, &write)) {
+      close(c_socket);
+      time = NA_REAL;
+    }
+
+    REAL(result)[i] = time;
+
+    close(c_socket);
+
+    /* Are we done? */
+
+    i++;
+    if (!continuous && i == count) { break; }
+
+    /* No, wait a bit then */
+
+    usleep((1000 - time) * 1000);
+  }
 
   UNPROTECT(1);
   return result;
