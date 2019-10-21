466bd74cf49b86fbcbf1e7128b54e863052f1493
Gabor Csardi <csardi.gabor@gmail.com>
2019-10-21 04:11:36 -0700

    Windows: use own inet_ntop implementation
    This is apparently a mess on Windows.

--- src/dns.c
+++ src/dns.c
@@ -1,7 +1,10 @@
 
 #ifdef _WIN32
 #include <Winsock2.h>
+#include <ws2tcpip.h>
 #endif
+
+#undef ERROR
 
 #include "pingr.h"
 #include "errors.h"
@@ -12,6 +15,124 @@
 #include <windns.h>
 
 #define AF_INET6 23
+#define NS_IN6ADDRSZ 16
+#define NS_INT16SZ 2
+
+// Copyright notice for inet_ntop4 and inet_ntop6
+
+/*
+ * Copyright (c) 1996-1999 by Internet Software Consortium.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+
+static const char *inet_ntop4 (const u_char *src, char *dst, size_t size) {
+  static const char fmt[] = "%u.%u.%u.%u";
+  char tmp[sizeof "255.255.255.255"];
+
+  if (sprintf(tmp, fmt, src[0], src[1], src[2], src[3]) >= size) {
+    R_THROW_ERROR("Cannot parse IPv4 address");
+  }
+  return strcpy(dst, tmp);
+}
+
+static const char *inet_ntop6 (const u_char *src, char *dst, size_t size) {
+  /*
+   * Note that int32_t and int16_t need only be "at least" large enough
+   * to contain a value of the specified size.  On some systems, like
+   * Crays, there is no such thing as an integer variable with 16 bits.
+   * Keep this in mind if you think this function should have been coded
+   * to use pointer overlays.  All the world's not a VAX.
+   */
+  char tmp[sizeof "ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255"], *tp;
+  struct { int base, len; } best, cur;
+  u_int words[NS_IN6ADDRSZ / NS_INT16SZ];
+  int i;
+
+  /*
+   * Preprocess:
+   *	Copy the input (bytewise) array into a wordwise array.
+   *	Find the longest run of 0x00's in src[] for :: shorthanding.
+   */
+  memset(words, '\0', sizeof words);
+  for (i = 0; i < NS_IN6ADDRSZ; i += 2)
+    words[i / 2] = (src[i] << 8) | src[i + 1];
+  best.base = -1;
+  cur.base = -1;
+  best.len = 0;
+  cur.len = 0;
+  for (i = 0; i < (NS_IN6ADDRSZ / NS_INT16SZ); i++) {
+    if (words[i] == 0) {
+      if (cur.base == -1)
+	cur.base = i, cur.len = 1;
+      else
+	cur.len++;
+    } else {
+      if (cur.base != -1) {
+	if (best.base == -1 || cur.len > best.len)
+	  best = cur;
+	cur.base = -1;
+      }
+    }
+  }
+  if (cur.base != -1) {
+    if (best.base == -1 || cur.len > best.len)
+      best = cur;
+  }
+  if (best.base != -1 && best.len < 2)
+    best.base = -1;
+
+  /*
+   * Format the result.
+   */
+  tp = tmp;
+  for (i = 0; i < (NS_IN6ADDRSZ / NS_INT16SZ); i++) {
+    /* Are we inside the best run of 0x00's? */
+    if (best.base != -1 && i >= best.base &&
+	i < (best.base + best.len)) {
+      if (i == best.base)
+	*tp++ = ':';
+      continue;
+    }
+    /* Are we following an initial run of 0x00s or any real hex? */
+    if (i != 0)
+      *tp++ = ':';
+    /* Is this address an encapsulated IPv4? */
+    if (i == 6 && best.base == 0 &&
+	(best.len == 6 || (best.len == 5 && words[5] == 0xffff))) {
+      if (!inet_ntop4(src+12, tp, sizeof tmp - (tp - tmp)))
+	return (NULL);
+      tp += strlen(tp);
+      break;
+    }
+    tp += sprintf(tp, "%x", words[i]);
+  }
+  /* Was it a trailing run of 0x00's? */
+  if (best.base != -1 && (best.base + best.len) ==
+      (NS_IN6ADDRSZ / NS_INT16SZ))
+    *tp++ = ':';
+  *tp++ = '\0';
+
+  /*
+   * Check for overflow, copy, and we're done.
+   */
+  if ((socklen_t)(tp - tmp) > size) {
+    R_THROW_ERROR("Cannot parse IPv6 address");
+  }
+  return strcpy(dst, tmp);
+}
 
 SEXP r_nsl(SEXP hostname, SEXP server, SEXP class, SEXP type) {
 
@@ -83,11 +204,11 @@ SEXP r_nsl(SEXP hostname, SEXP server, SEXP class, SEXP type) {
 
     switch(ptr->wType) {
     case DNS_TYPE_A:
-      inet_ntoa(AF_INET, &(ptr->Data.A.IpAddress), buf, sizeof buf);
+      inet_ntop4((u_char*) &(ptr->Data.A.IpAddress), buf, sizeof buf);
       break;
 
     case DNS_TYPE_AAAA:
-      inet_ntoa(AF_INET6, &(ptr->Data.AAAA.Ip6Address), buf, sizeof buf);
+      inet_ntop6((u_char*) &(ptr->Data.AAAA.Ip6Address), buf, sizeof buf);
       break;
 
     case DNS_TYPE_NS:
