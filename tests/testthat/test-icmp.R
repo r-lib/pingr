8fa1642787141eedd0ef88afcd7c2f9c9f26a07f
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-07 01:40:13 +0100

    Trying to write more robust ICMP tests
    - better IP that always fails
    - longer timeouts for pings

--- tests/testthat/test-icmp.R
+++ tests/testthat/test-icmp.R
@@ -1,32 +1,29 @@
 test_that("We can ping localhost", {
-
-  if (Sys.getenv("APPVEYOR") == "") {
-    pr <- ping("127.0.0.1", count = 1)
-    expect_true(is.double(pr))
-    expect_true(length(pr) == 1)
-    expect_true(pr < 1000)
-  }
+  pr <- ping("127.0.0.1", count = 1)
+  expect_true(is.double(pr))
+  expect_true(length(pr) == 1)
+  expect_true(pr < 5000)
 })
 
 test_that("We can ping a remote host", {
 
-  if (Sys.getenv("APPVEYOR") == "") {
+  ## Non-existent IP
+  pr <- ping("0.0.0.1", count = 1)
+  print(pr)
+  expect_equal(pr, NA_real_)
 
-    ## Non-existent IP
-    pr <- ping("192.0.2.1", count = 1)
-    expect_equal(pr, NA_real_)
+  ## Google
+  pr <- ping("google.com", count = 1, timeout = 5.0)
+  expect_true(is.double(pr))
+  expect_true(length(pr) == 1)
+  print(pr)
+  expect_true(pr < 5000)
 
-    ## Google
-    pr <- ping("google.com", count = 1)
-    expect_true(is.double(pr))
-    expect_true(length(pr) == 1)
-    expect_true(pr < 1000)
-
-    pr <- ping("8.8.8.8", count = 1)
-    expect_true(is.double(pr))
-    expect_true(length(pr) == 1)
-    expect_true(pr < 1000)
-  }
+  pr <- ping("8.8.8.8", count = 1, timeout = 5.0)
+  expect_true(is.double(pr))
+  expect_true(length(pr) == 1)
+  print(pr)
+  expect_true(pr < 5000)
 })
 
 test_that("We don't wait too long", {
