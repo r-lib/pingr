10f4f33d23a0817724b529306a186ed7454db0e1
Colin Rundel <rundel@gmail.com>
2025-09-19 15:34:21 -0400

    Update

--- tests/testthat/_snaps/nsl.md
+++
@@ -1,9 +0,0 @@
-# nsl function works with SRV records
-
-    Code
-      result$answer$data
-    Output
-      [[1]]
-      [1] "10 100 80 mxtoolbox.com"
-      
-
--- tests/testthat/test-nsl.R
+++ tests/testthat/test-nsl.R
@@ -2,5 +2,5 @@ test_that("nsl function works with SRV records", {
   testthat::skip_on_ci()
 
   result = nsl("_http._tcp.mxtoolbox.com", type = 33L)
-  expect_snapshot(result$answer$data)
+  expect_true("10 100 80 mxtoolbox.com" %in% unlist(result$answer$data))
 })
